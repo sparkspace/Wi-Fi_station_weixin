d21a95cbfdfbe7182dda041804fa47378d50ca88
wcsngonion <1010136561@qq.com>
2017-11-10 02:21:08 +0800

    update

---
+++ 数据库练习/database.js
@@ -0,0 +1,75 @@
+
+var model = require("./model");
+
+/**
+ * 插入
+ */
+function insert() {
+ 
+    var user = new model({
+        username : 'Tracy McGrady',                 //用户账号
+        userpwd: 'abcd',                            //密码
+        userage: 37,                                //年龄
+        logindate : new Date()                      //最近登录时间
+    });
+
+    user.save(function (err, res) {
+
+        if (err) {
+            console.log("Error:" + err);
+        }
+        else {
+            console.log("Res:" + res);
+        }
+
+    });
+}
+
+insert();
+
+function update(){
+    var wherestr = {'username' : 'Tracy McGrady'};
+    var updatestr = {'userpwd': 'zzzz'};
+    
+    User.update(wherestr, updatestr, function(err, res){
+        if (err) {
+            console.log("Error:" + err);
+        }
+        else {
+            console.log("Res:" + res);
+        }
+    })
+}
+
+update();
+
+
+function del(){
+    var wherestr = {'username' : 'Tracy McGrady'};
+    
+    User.remove(wherestr, function(err, res){
+        if (err) {
+            console.log("Error:" + err);
+        }
+        else {
+            console.log("Res:" + res);
+        }
+    })
+}
+
+del();
+
+function getByConditions(){
+    var wherestr = {'username' : 'Tracy McGrady'};
+    
+    User.find(wherestr, function(err, res){
+        if (err) {
+            console.log("Error:" + err);
+        }
+        else {
+            console.log("Res:" + res);
+        }
+    })
+}
+
+getByConditions();
